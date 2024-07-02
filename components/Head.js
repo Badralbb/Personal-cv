d50ed5d5a04c188b4467fd36d0647acc288dafe2
Badralbb <badralbaigalmaa7@gmail.com>
2024-07-02 15:34:45 +0800

    initial

---
+++ components/Head.js
@@ -0,0 +1,16 @@
+export function Head() {
+    return (
+       
+        <div className="flex justify-between items-center">
+            <div>&lt;ss/&gt;</div>
+            <div>
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 18H26" stroke="#4B5563" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10 12H26" stroke="#4B5563" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10 24H26" stroke="#4B5563" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+            </div>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+
+import { Head } from "@/components/Head";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="w-[343px] m-auto">
+        <Head />
+       
+      </section>
+     
+    </main>
+  );
+}
