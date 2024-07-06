c1dee7df43d01ec8ed3cb3c72be4e772f4a0418a
Badralbb <badralbaigalmaa7@gmail.com>
2024-07-06 23:37:35 +0800

    style change

--- components/Upwork.js
+++ components/Upwork.js
@@ -1,31 +1,37 @@
-export const Upwork = ({props})=>{
-    const items = props.listItems
-    return ( <div className="flex flex-col gap-12 mt-12">
-        
-
-    <div className="bg-[#FFFFFF] dark:bg-[#1F2937] w-[896px] m-auto rounded-xl border-1 shadow-black shadow-md p-[32px]">
-
+export const Upwork = ({ props }) => {
+  const items = props.listItems;
+  return (
+    <div className="flex flex-col gap-12 mt-12">
+      <div className="bg-[#FFFFFF] dark:bg-[#1F2937] w-[896px] m-auto rounded-xl border-1 shadow-black shadow-md p-[32px]">
         <div className="m-auto flex justify-between">
-            <div><img src="images/logo-upwork.png" /></div>
-            
-            <div className="w-[384px]">
-                <div className="flex justify-between">
-                    <h2 className="text-[#111827] text-lg dark:text-[#F9FAFB]">{props.title}</h2>
-                    
-                </div>
+          <div>
+            <img src="images/logo-upwork.png" />
+          </div>
 
-                <div className="mt-4">
-                    <ul className="list-disc  ml-5 text-[#4B5563] text-base flex flex-col gap-1 flex-wrap">
-                    {items.map((item,index) =>(<li key={index}>{item}</li>))}
-                        
-                    </ul>
-                </div>
+          <div className="w-[384px]">
+            <div className="flex justify-between">
+              <h2 className="text-[#111827] text-lg dark:text-[#F9FAFB]">
+                {props.title}
+              </h2>
             </div>
 
-            <h3 className="text-[#374151] text-base text-nowrap dark:text-[#D1D5DB]">{props.startDate}</h3>
+            <div className="mt-4">
+              <ul
+                id="list"
+                className="list-disc  ml-5 text-[#4B5563] text-base flex flex-col gap-1 flex-wrap"
+              >
+                {items.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <h3 className="text-[#374151] text-base text-nowrap dark:text-[#D1D5DB]">
+            {props.startDate}
+          </h3>
         </div>
+      </div>
     </div>
-    
-    
-</div>)
-}
+  );
+};
